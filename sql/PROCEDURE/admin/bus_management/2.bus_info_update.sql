c9cc50840ea176472b4afa48ce806dd7ee0f0810
iamyourkeyring <181794569+iamxoghks@users.noreply.github.com>
2025-12-05 12:12:31 +0900

    bus schedule cancel

--- sql/PROCEDURE/admin/bus_management/2.bus_info_update.sql
+++ sql/PROCEDURE/admin/bus_management/2.bus_info_update.sql
@@ -20,3 +20,18 @@ DELIMITER ;
 ----- procedure call -----
 -- bus_id 5번의 버스의 ground_id 를 20으로 변경하고, 좌석 수는 변경하지 않을 때
 CALL update_bus_info(5, 20, NULL);
+
+
+----- 버스 사용/미사용 상태 변경 -----
+DELIMITER //
+
+CREATE PROCEDURE change_bus_active(
+    IN p_bus_id BIGINT
+)
+BEGIN
+    UPDATE bus_list
+    SET is_active = 1 - is_active
+    WHERE bus_id = p_bus_id;
+END //
+
+DELIMITER ;
