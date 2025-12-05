71a3e991b1087da14f092484be7180169091779e
lampshub <dkdkwlsgml@gmail.com>
2025-12-05 11:29:54 +0900

    add

--- sql/DML/search/game_search.sql
+++ sql/DML/search/game_search.sql
@@ -20,3 +20,19 @@ inner join team_list away_t
     where home_t.team_name='삼성' and gs.game_date='2025-10-01 17:00:00';
 
 -- GLIST-02 경기 예매 상황 실시간 조회 --
+-- 홈팀명, 에워팀명, 경기 시작 시간, 경기별 판매된 좌석수, 남은 좌석수, 전체 좌석수
+select 
+    ht.team_name home_team,
+    at.team_name away_team,
+    g_s.game_date,
+    sum(g_r.game_res_count) sold_seats,
+    ( select count(*) from seat_list s WHERE s.ground_id = g_s.ground_id) -  SUM(g_r.game_res_count) remaining_seats,    
+    ( select count(*) from seat_list s where s.ground_id = g_s.ground_id ) total_seats
+from game_res_list g_r
+inner join game_schedule_list g_s
+    on g_r.game_id = g_s.game_id
+left join team_list ht
+    on g_s.h_team_id = ht.team_id
+left join team_list at
+    on g_s.a_team_id = at.team_id
+group by g_s.game_id;
