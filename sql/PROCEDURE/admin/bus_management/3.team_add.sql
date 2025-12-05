564429503fe798d4b0207bb949999b6dff023aeb
yune22 <wjddbs020604@gmail.com>
2025-12-05 10:45:01 +0900

    team add

---
+++ sql/PROCEDURE/admin/bus_management/3.team_add.sql
@@ -0,0 +1,7 @@
+delimiter //
+create procedure team_add(in ground_Id bigint, in admin_Id varchar(36), in team_Name varchar(255))
+begin
+	insert into team_list(ground_id, admin_id, team_name)
+    values (ground_Id, admin_Id, team_Name);
+end
+// delimiter ;
